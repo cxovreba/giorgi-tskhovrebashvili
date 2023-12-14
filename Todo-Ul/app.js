87e9bdce4de395b0e29bbabc406109fcecc1af6b
giorgi tskhovrebashvili <gio.cxovrebashvili@gmail.com>
2023-12-14 19:39:28 +0400

    Js first stage

--- Todo-Ul/app.js
+++ Todo-Ul/app.js
@@ -1,17 +1,31 @@
-const add = document.querySelector('.btn');
-const ul = document.querySelector('.ul');
-
 const date = new Date();
-date.getDate();
-date.getTime();
+    date.getDate();
+    date.getTime();
+
+
+const add = document.querySelector('.btn');
+const ul = document.querySelector('#ul');
+
+let counter = 0;
 
 const generateListItem = val => {
     const li = document.createElement('li');
-    li.innerHTML = `${val} <input type="checkbox"/> <button class="btn1"></button>`;
+    const div = document.createElement('div');
+    li.classList.add(`li-${counter++}`);
+    li.innerHTML = `${val}`;
     ul.appendChild(li);
+    div.innerHTML = `<input class="checkbox" type="button"/> <button class="btn1" onclick="Delete(this)"></button>`;
+    li.appendChild(div);
 }
 
 add.addEventListener('click', () => {
     const input = document.querySelector('input');
     generateListItem(input.value);
 })
+
+function Delete(btn) {
+    const listItem = btn.closest('li');
+    if (listItem) {
+        ul.removeChild(listItem);
+    }
+}
