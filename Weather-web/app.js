954d4b7bf6ad658b323ddbfeb5b74cae525c9da1
giorgi tskhovrebashvili <gio.cxovrebashvili@gmail.com>
2023-12-25 15:12:13 +0400

    javascript

--- Weather-web/app.js
+++ Weather-web/app.js
@@ -1,8 +1,7 @@
 setInterval(() => {
-    document.getElementById('time').innerText = new Date().toLocaleTimeString('en-Us', {timeStyle: 'short'});
-    document.getElementById('day').innerText = new Date().toLocaleDateString('en-US', {weekday: 'long'});
-    document.getElementById('date').innerText = new Date().toLocaleDateString('en-US', {dateStyle: 'medium'});
+    const timeDate = document.querySelector('.time-date');
+    timeDate.innerText = `${new Date().toLocaleTimeString('en-Us', {timeStyle: 'short'})} - ${new Date().toLocaleDateString('en-US', {weekday: 'long'})},  ${new Date().toLocaleDateString('en-US', {dateStyle: 'medium'})}`;
+    // document.getElementById('time').innerText = new Date().toLocaleTimeString('en-Us', {timeStyle: 'short'});
+    // document.getElementById('day').innerText = new Date().toLocaleDateString('en-US', {weekday: 'long'});
+    // document.getElementById('date').innerText = new Date().toLocaleDateString('en-US', {dateStyle: 'medium'});
 }, 1000);
-
-const timeDate = document.querySelector('.time-date');
-timeDate.innerText = `${new Date().toLocaleTimeString('en-Us', {timeStyle: 'short'})} - ${new Date().toLocaleDateString('en-US', {weekday: 'long'})}  ${new Date().toLocaleDateString('en-US', {dateStyle: 'medium'})}`;
