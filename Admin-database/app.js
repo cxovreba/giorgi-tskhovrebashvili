fdf252538fccdb7bda2079ef9e4a07660fa4eb8e
giorgi tskhovrebashvili <gio.cxovrebashvili@gmail.com>
2023-12-26 22:20:20 +0400

    work done

--- Admin-database/app.js
+++ Admin-database/app.js
@@ -16,7 +16,8 @@ const initData = async () => {
         table.innerHTML = `<tr>
             <th><input type="checkbox"></th>
             <th id="title">
-                Title 
+                Title
+                <input id="search" type="text" placeholder="search name..."> 
                 <div class="button">
                     <button id="button1"><img src="./ant-design_caret-up-filled.png" alt=""></button>
                     <button id="button2"><img src="./ant-design_caret-down-filled.png" alt=""></button>
@@ -58,6 +59,37 @@ const initData = async () => {
 
     document.getElementById('button1').addEventListener("click", sortData);
     document.getElementById('button2').addEventListener("click", reverseData);
+
+    const search = document.getElementById("search");
+
+    const filterData = (searchTerm) => {
+        const filteredData = data.filter(course =>
+            course.Title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+  
+        const filteredHTML = filteredData.map(course => `
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>${course.Title}</td>
+                <td>${course.Date}</td>
+                <td>${course.Title1}</td>
+                <td class="amount">${course.Amount}</td>
+                <td class="status">${course.Status}</td>
+            </tr>
+        `).join('');
+
+        const tableRows = table.querySelectorAll('tr');
+        for (let i = 1; i < tableRows.length; i++) {
+            tableRows[i].remove();
+        }
+        
+        table.insertAdjacentHTML('beforeend', filteredHTML);
+    };
+
+    search.addEventListener("input", () => {
+        const searchTerm = search.value.trim();
+        filterData(searchTerm);
+    });
 };
 
 initData();
