f2745d7e57af39cb0f84ed6f5aa01caa3d36fbc0
giorgi tskhovrebashvili <gio.cxovrebashvili@gmail.com>
2023-12-29 22:35:35 +0400

    new updates done

--- Todo-Ul/app.js
+++ Todo-Ul/app.js
@@ -1,5 +1,5 @@
 const add = document.querySelector('.btn');
-const ul = document.querySelector('#ul');
+const input = document.querySelector('input');
 
 setInterval(() => {
     document.getElementById('currentTime').innerText = new Date().toLocaleTimeString('en-Us', {timeStyle: 'short'});
@@ -7,34 +7,53 @@ setInterval(() => {
     document.getElementById('currentDate').innerText = new Date().toLocaleDateString('en-US', {day: 'numeric'});
 }, 1000);
 
-let counter = 0;
+const data = localStorage.getItem('data');
+let list = data ? JSON.parse(data) : [];
 
-const generateListItem = val => {
-    const li = document.createElement('li');
-    const div = document.createElement('div');
-    const p = document.createElement('p');
-    li.classList.add(`li-${counter++}`);
-    li.innerHTML = `${val}`;
-    ul.appendChild(li);
-    div.innerHTML = `<input class="checkbox" type="checkbox"/> <button class="btn1" onclick="Delete(this)"></button>`;
-    li.appendChild(p);
-    li.appendChild(div);
-    p.innerHTML = `${new Date().toLocaleTimeString('en-Us', {timeStyle: 'short'})}`;
+function generateListItem() {
+    const ul = document.querySelector('#ul');
+    if (list.length) {
+        ul.innerHTML = '';
+        list.forEach((el, i) => {
+            const li = document.createElement('li');
+            li.innerText = el;
+            const div = document.createElement('div');
+            const p = document.createElement('p');
+            li.innerHTML = `${el.value}`;
+            p.innerHTML = `${el.date}`;
+            div.innerHTML = `<input id="checkbox" type="checkbox" ${ el.isChecked ? 'checked' : null} onchange="check(${i})"/> <button id="btn1" onclick="DeleteItem(${i})"></button>`;
+            ul.appendChild(li);
+            li.appendChild(p);
+            li.appendChild(div);
+        });
+        return;
+    }
+    ul.innerHTML = '';
+}
+
+generateListItem();
+
+function check(i) {
+    list[i].isChecked = !list[i].isChecked;
+    updateStorage(list);
+    generateListItem();
+}
+
+function DeleteItem(i) {
+    list.splice(i, 1);
+    updateStorage(list);
+    generateListItem();
 }
 
 add.addEventListener('click', () => {
-    const input = document.querySelector('input');
-    generateListItem(input.value);
-    input.value = '';
+    let {value} = document.querySelector('#note');
+    const date = new Date().toLocaleTimeString('en-Us', {timeStyle: 'short'});
+    list.push({ value, date, isChecked: false });
+    document.querySelector('#note').value = '';
+    updateStorage(list);
+    generateListItem();
 })
 
-input.addEventListener('blur', () => {
-    this.classList.add('typing');
-})
-
-function Delete(btn) {
-    const listItem = btn.closest('li');
-    if (listItem) {
-        ul.removeChild(listItem);
-    }
-}
+function updateStorage(list) {
+    localStorage.setItem('data', JSON.stringify(list));
+};
